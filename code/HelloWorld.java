d886a344cf6c5c93158ad857738c85f0827aab51
LHK_AI <2628612457@qq.com>
2019-01-01 16:21:32 +0800

    Completed HelloWorld.java

---
+++ code/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+     public static void main(String[] args) {
+        int year = 2000;
+        if(year%4==0){
+            System.out.println(year +" is a leap year");
+        }
+        //System.out.println("Hello,World!");
+        
+    }
+}
